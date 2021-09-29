5a8f88dfe62e4c4f802c013f3021393c55141133
ChrisFerrer23145 <70284666+ChrisFerrer23145@users.noreply.github.com>
2021-09-29 13:50:03 -0400

    Update Base.java

--- Base.java
+++ Base.java
@@ -1,66 +1,29 @@
-package com.columbushs.unit2;
+public class Base {
 
-public class Unit2_1External {
-	
-	//constructors
-	String constName;
-	int constAge;
-	double constDecimal;
-//	private String constName;
-//	private int constAge;
-//	private double constDecimal;
-
-
-	public Unit2_1External(String constName, int constAge, double constDecimal){
-	  
-	  System.out.println("Object Created with constructor 1 Name, Age, miles");
-	}
-
-
-
-	public Unit2_1External(String cn, int ca){
-	  constName = cn;
-	  constAge = ca;
-	  constDecimal = 0;
-	  System.out.println("Object Created with constructor 2 Name, Age");
-
-	}
-
-	public Unit2_1External(){
-	  constName = null;
-	  constAge = 0;
-	  constDecimal = 0;
-	  System.out.println("Object Created with constructor 3 empty");
-
-	}
-
-
-	public void unit2_1Method(String name, int age, int milesRun ){
-	   
-	    System.out.println("We're using the method with milesRun");
-	    System.out.println(name);
-	    System.out.println(age);
-	    System.out.println(milesRun);
-
-	 }
-
-	 public void unit2_1Method(String name, int age){
-
-	   System.out.println("We're using the method without milesRun");
-	    System.out.println(name);
-	    System.out.println(age);
-
-
-	 }
-	 
-	 public void printName(){
-
-		   System.out.println("This is the name: "+constName);
-
-
-
-		 }
-
-	}
+    static int Addition(String equation){
+        int point = equation.indexOf("+");
+        int st1 = Integer.parseInt(equation.substring(0, point));
+        int st2 = Integer.parseInt(equation.substring(point + 1));
+        return((st1 + st2));
+    }
+    static int Subtraction(String equation){
+        int point = equation.indexOf("-");
+        int st1 = Integer.parseInt(equation.substring(0, point));
+        int st2 = Integer.parseInt(equation.substring(point + 1));
+        return((st1 - st2));
+    }
+    static int Multiplication(String equation){
+        int point = equation.indexOf("*");
+        int st1 = Integer.parseInt(equation.substring(0, point));
+        int st2 = Integer.parseInt(equation.substring(point + 1));
+        return((st1 * st2));
+    }
+    static double Division(String equation){
+        int point = equation.indexOf("/");
+        double st1 = Integer.parseInt(equation.substring(0, point));
+        double st2 = Integer.parseInt(equation.substring(point + 1));
+        return((st1/st2));
+    }
+}
 
 
