6ab6c44cffa602bafcde20ad1e6aece647a59abe
ChrisFerrer23145 <70284666+ChrisFerrer23145@users.noreply.github.com>
2021-10-01 10:57:22 -0400

    Update Extra.java

--- calculator/Extra.java
+++ calculator/Extra.java
@@ -1,7 +1,13 @@
 public class Extra {
-	static double Sqrt(String equation){
+    static double Sqrt(String equation){
         int point = equation.indexOf(".");
         double st2 = Integer.parseInt(equation.substring(point + 1));
         return((Math.sqrt(st2)));
+    }
+    static int Exponent(String equation){
+        int point = equation.indexOf("^");
+        int st1 = Integer.parseInt(equation.substring(0, point));
+        int st2 = Integer.parseInt(equation.substring(point + 1));
+        return((Math.pow(st1, st2)));
     }
 }
