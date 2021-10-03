26cacb45b30e1d0d0674dc8d788f5adb037e8365
ChrisFerrer23145 <70284666+ChrisFerrer23145@users.noreply.github.com>
2021-10-02 20:53:19 -0400

    Update Components.java

--- calculator/Components.java
+++ calculator/Components.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class Components {
-    public static void Handle(String equation, int num) {
-       
+    public static void Handle(String equation, int num, String order) {
+        
     }
+    public static 
 }
