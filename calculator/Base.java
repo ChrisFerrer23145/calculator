27faffce479c2723e9816b3769ddc7a85fa5c3db
ChrisFerrer23145 <70284666+ChrisFerrer23145@users.noreply.github.com>
2021-10-04 06:20:52 -0400

    Update Base.java

--- calculator/Base.java
+++ calculator/Base.java
@@ -1,23 +1,23 @@
 public class Base {
-    static int Addition(String equation){
+    int Addition(String equation){
         int point = equation.indexOf("+");
         int st1 = Integer.parseInt(equation.substring(0, point));
         int st2 = Integer.parseInt(equation.substring(point + 1));
         return((st1 + st2));
     }
-    static int Subtraction(String equation){
+    int Subtraction(String equation){
         int point = equation.indexOf("-");
         int st1 = Integer.parseInt(equation.substring(0, point));
         int st2 = Integer.parseInt(equation.substring(point + 1));
         return((st1 - st2));
     }
-    static int Multiplication(String equation){
+    int Multiplication(String equation){
         int point = equation.indexOf("*");
         int st1 = Integer.parseInt(equation.substring(0, point));
         int st2 = Integer.parseInt(equation.substring(point + 1));
         return((st1 * st2));
     }
-    static double Division(String equation){
+    double Division(String equation){
         int point = equation.indexOf("/");
         double st1 = Integer.parseInt(equation.substring(0, point));
         double st2 = Integer.parseInt(equation.substring(point + 1));
