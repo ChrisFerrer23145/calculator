69b96b274efeeb55b56370cfb015b573b0cdbab1
ChrisFerrer23145 <70284666+ChrisFerrer23145@users.noreply.github.com>
2021-10-05 11:21:53 -0400

    Update Extra.java

--- calculator/Extra.java
+++ calculator/Extra.java
@@ -1,6 +1,6 @@
 public class Extra {
     double Sqrt(String equation){
-        int point = equation.indexOf(".");
+        int point = equation.indexOf("%");
         double st2 = Integer.parseInt(equation.substring(point + 1));
         return((Math.sqrt(st2)));
     }
@@ -9,5 +9,29 @@ public class Extra {
         int st1 = Integer.parseInt(equation.substring(0, point));
         int st2 = Integer.parseInt(equation.substring(point + 1));
         return((Math.pow(st1, st2)));
+    }
+    double Addition(String equation){
+        int point = equation.indexOf("+");
+        int st1 = Double.parseDouble(equation.substring(0, point));
+        int st2 = Double.parseDouble(equation.substring(point + 1));
+        return((st1 + st2));
+    }
+    double Subtraction(String equation){
+        int point = equation.indexOf("-");
+        int st1 = Double.parseDouble(equation.substring(0, point));
+        int st2 = Double.parseDouble(equation.substring(point + 1));
+        return((st1 - st2));
+    }
+    double Multiplication(String equation){
+        int point = equation.indexOf("*");
+        int st1 = Double.parseDouble(equation.substring(0, point));
+        int st2 = Double.parseDouble(equation.substring(point + 1));
+        return((st1 * st2));
+    }
+    double Division(String equation){
+        int point = equation.indexOf("/");
+        double st1 = Double.parseDouble(equation.substring(0, point));
+        double st2 = Double.parseDouble(equation.substring(point + 1));
+        return((st1/st2));
     }
 }
