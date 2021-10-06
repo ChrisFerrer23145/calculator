43c855ce72783410ed88ec457db8770c28105a1e
ChrisFerrer23145 <70284666+ChrisFerrer23145@users.noreply.github.com>
2021-10-05 21:35:18 -0400

    Update Extra.java

--- calculator/Extra.java
+++ calculator/Extra.java
@@ -12,20 +12,20 @@ public class Extra {
     }
     double Addition(String equation){
         int point = equation.indexOf("+");
-        int st1 = Double.parseDouble(equation.substring(0, point));
-        int st2 = Double.parseDouble(equation.substring(point + 1));
+        double st1 = Double.parseDouble(equation.substring(0, point));
+        double st2 = Double.parseDouble(equation.substring(point + 1));
         return((st1 + st2));
     }
     double Subtraction(String equation){
         int point = equation.indexOf("-");
-        int st1 = Double.parseDouble(equation.substring(0, point));
-        int st2 = Double.parseDouble(equation.substring(point + 1));
+        double st1 = Double.parseDouble(equation.substring(0, point));
+        double st2 = Double.parseDouble(equation.substring(point + 1));
         return((st1 - st2));
     }
     double Multiplication(String equation){
         int point = equation.indexOf("*");
-        int st1 = Double.parseDouble(equation.substring(0, point));
-        int st2 = Double.parseDouble(equation.substring(point + 1));
+        double st1 = Double.parseDouble(equation.substring(0, point));
+        double st2 = Double.parseDouble(equation.substring(point + 1));
         return((st1 * st2));
     }
     double Division(String equation){
