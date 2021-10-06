12d6c7e4f786d94fe10932335278f631e22306d6
ChrisFerrer23145 <70284666+ChrisFerrer23145@users.noreply.github.com>
2021-10-05 21:30:55 -0400

    Update Components.java

--- calculator/Components.java
+++ calculator/Components.java
@@ -18,35 +18,36 @@ public class Components {
         Base b = new Base();
         String[] flags = {"%", "^", "*", "/", "+", "-"};
         for (i=0;i<order.length();i++) {
-            int t1 = -1;
-            if (Character.toString(order.charAt(i)).equals(flags[0])) {
+            if (equation.contains(flags[0])) {
                 int test = equation.indexOf("%");
-                int t2 = equation.indexOf(order.charAt(i+1));
-                double n = e.Sqrt(equation.substring(test, t2));
-                equation = equation.substring(0, test) + n + equation.substring(t2);
-                System.out.println(equation);
-            } else if (Character.toString(order.charAt(i)).equals(flags[1])) {
+                int t2 = order.indexOf("%");
+                if (t2 == order.length() - 1) {
+                    double n = e.Sqrt(equation.substring(test + 1));
+                    equation = equation.substring(0, test) + n;
+                } else {
+                    String t1 = Character.toString(order.charAt(t2 + 1));
+                    int t3 = equation.indexOf(t1, test);
+                    double n = e.Sqrt(equation.substring(test + 1, t3));
+                    equation = equation.substring(0, test) + n + equation.substring(t3);
+                }
+            } else if (equation.contains(flags[1])) {
                 int test = equation.indexOf("^");
-                if (test != 0) {
-                    t1 = equation.indexOf(order.charAt(i-1));
+                int t2 = order.indexOf("^");
+                if (t2 == order.length() - 1) {
+                    String t1 = Character.toString(order.charAt(t2 - 1));
+                    int t3 = equation.indexOf(t1);
+                    double n = e.Exponent(equation.substring(t3 + 1));
+                    equation = equation.substring(0, t3 + 1) + n;
+                } else {
+                    String t1 = Character.toString(order.charAt(t2 + 1));
+                    int t3 = equation.indexOf(t1, test);
+                    double n = e.Exponent(equation.substring(test + 1, t3));
+                    equation = equation.substring(0, test) + n + equation.substring(t3);
                 }
-                int t2 = equation.indexOf(order.charAt(i+1));
-                double n = e.Exponent(equation.substring(t1 + 1, t2));
-                equation = equation.substring(0, test) + n + equation.substring(t2);
-                System.out.println(equation);
-            } else if (Character.toString(order.charAt(i)).equals(flags[2])) {
-                int test = equation.indexOf("*");
-                if (test != 0) {
-                    t1 = equation.indexOf(order.charAt(i-1));
-                }
-                int t2 = equation.indexOf(order.charAt(i+1));
-                double n = b.Multiplication(equation.substring(t1 + 1, t2));
-                equation = equation.substring(0, test) + n + equation.substring(t2);
-                System.out.println(equation);
             }
             order = order.substring(0, i) + order.substring(i + 1);
         }
         
-        return "error occurred";
+        return equation;
     }
 }
