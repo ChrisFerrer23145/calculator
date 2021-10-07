a32151f1617e227ce15dca4fe30ec245fc6af4ff
ChrisFerrer23145 <70284666+ChrisFerrer23145@users.noreply.github.com>
2021-10-07 12:35:45 -0400

    Update Extra.java

--- calculator/Extra.java
+++ calculator/Extra.java
@@ -1,27 +1,33 @@
 public class Extra {
     double Sqrt(String equation){
+        String eq2;
+        double st2;
         int point = equation.indexOf("%");
         if (!equation.substring(point + 1).contains(".")) {
-            String eq2 = equation.substring(point + 1) + ".0";
-            double st2 = Double.parseDouble(eq2);
+            eq2 = equation.substring(point + 1) + ".0";
+            st2 = Double.parseDouble(eq2);
         } else {
-            double st2 = Double.parseDouble(equation.substring(point + 1));
+            st2 = Double.parseDouble(equation.substring(point + 1));
         }
         return((Math.sqrt(st2)));
     }
     double Exponent(String equation){
         int point = equation.indexOf("^");
+        String eq1;
+        String eq2;
+        double st1;
+        double st2;
         if (!equation.substring(0, point).contains(".")) {
-            String eq1 = equation.substring(0, point) + ".0";
-            double st1 = Double.parseDouble(eq2);
+            eq1 = equation.substring(0, point) + ".0";
+            st1 = Double.parseDouble(eq1);
         } else {
-            double st1 = Double.parseDouble(equation.substring(point + 1));
+            st1 = Double.parseDouble(equation.substring(point + 1));
         }
         if (!equation.substring(point + 1).contains(".")) {
-            String eq2 = equation.substring(point + 1) + ".0";
-            double st2 = Double.parseDouble(eq2);
+            eq2 = equation.substring(point + 1) + ".0";
+            st2 = Double.parseDouble(eq2);
         } else {
-            double st2 = Double.parseDouble(equation.substring(point + 1));
+            st2 = Double.parseDouble(equation.substring(point + 1));
         }
         return((Math.pow(st1, st2)));
     }
