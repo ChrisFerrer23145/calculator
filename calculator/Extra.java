cd937e1c07930461359d0b8dc94062129459caa0
ChrisFerrer23145 <70284666+ChrisFerrer23145@users.noreply.github.com>
2021-10-07 12:21:38 -0400

    Update Extra.java

--- calculator/Extra.java
+++ calculator/Extra.java
@@ -1,37 +1,28 @@
 public class Extra {
     double Sqrt(String equation){
         int point = equation.indexOf("%");
-        double st2 = Integer.parseInt(equation.substring(point + 1));
+        if !(equation.substring(point + 1).contains(".")) {
+            String eq2 = equation.substring(point + 1) + ".0"
+            double st2 = Double.parseDouble(eq2);
+        } else {
+            double st2 = Double.parseDouble(equation.substring(point + 1));
+        }
         return((Math.sqrt(st2)));
     }
     double Exponent(String equation){
         int point = equation.indexOf("^");
-        int st1 = Integer.parseInt(equation.substring(0, point));
-        int st2 = Integer.parseInt(equation.substring(point+1));
+        if !(equation.substring(0, point).contains(".")) {
+            String eq1 = equation.substring(0, point) + ".0"
+            double st1 = Double.parseDouble(eq2);
+        } else {
+            double st1 = Double.parseDouble(equation.substring(point + 1));
+        }
+        if !(equation.substring(point + 1).contains(".")) {
+            String eq2 = equation.substring(point + 1) + ".0"
+            double st2 = Double.parseDouble(eq2);
+        } else {
+            double st2 = Double.parseDouble(equation.substring(point + 1));
+        }
         return((Math.pow(st1, st2)));
-    }
-    double Addition(String equation){
-        int point = equation.indexOf("+");
-        double st1 = Double.parseDouble(equation.substring(0, point));
-        double st2 = Double.parseDouble(equation.substring(point + 1));
-        return((st1 + st2));
-    }
-    double Subtraction(String equation){
-        int point = equation.indexOf("-");
-        double st1 = Double.parseDouble(equation.substring(0, point));
-        double st2 = Double.parseDouble(equation.substring(point + 1));
-        return((st1 - st2));
-    }
-    double Multiplication(String equation){
-        int point = equation.indexOf("*");
-        double st1 = Double.parseDouble(equation.substring(0, point));
-        double st2 = Double.parseDouble(equation.substring(point + 1));
-        return((st1 * st2));
-    }
-    double Division(String equation){
-        int point = equation.indexOf("/");
-        double st1 = Double.parseDouble(equation.substring(0, point));
-        double st2 = Double.parseDouble(equation.substring(point + 1));
-        return((st1/st2));
     }
 }
