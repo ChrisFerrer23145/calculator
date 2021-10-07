f578e2763b24c650ee7bdd679f51589edfe26cb6
ChrisFerrer23145 <70284666+ChrisFerrer23145@users.noreply.github.com>
2021-10-07 12:25:55 -0400

    Update Extra.java

--- calculator/Extra.java
+++ calculator/Extra.java
@@ -1,8 +1,8 @@
 public class Extra {
     double Sqrt(String equation){
         int point = equation.indexOf("%");
-        if !(equation.substring(point + 1).contains(".")) {
-            String eq2 = equation.substring(point + 1) + ".0"
+        if (!equation.substring(point + 1).contains(".")) {
+            String eq2 = equation.substring(point + 1) + ".0";
             double st2 = Double.parseDouble(eq2);
         } else {
             double st2 = Double.parseDouble(equation.substring(point + 1));
@@ -11,14 +11,14 @@ public class Extra {
     }
     double Exponent(String equation){
         int point = equation.indexOf("^");
-        if !(equation.substring(0, point).contains(".")) {
-            String eq1 = equation.substring(0, point) + ".0"
+        if (!equation.substring(0, point).contains(".")) {
+            String eq1 = equation.substring(0, point) + ".0";
             double st1 = Double.parseDouble(eq2);
         } else {
             double st1 = Double.parseDouble(equation.substring(point + 1));
         }
-        if !(equation.substring(point + 1).contains(".")) {
-            String eq2 = equation.substring(point + 1) + ".0"
+        if (!equation.substring(point + 1).contains(".")) {
+            String eq2 = equation.substring(point + 1) + ".0";
             double st2 = Double.parseDouble(eq2);
         } else {
             double st2 = Double.parseDouble(equation.substring(point + 1));
