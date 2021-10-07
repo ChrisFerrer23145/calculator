b1bb49a8728c5bdafe0d9e1337ba4c8df7112806
ChrisFerrer23145 <70284666+ChrisFerrer23145@users.noreply.github.com>
2021-10-07 12:36:00 -0400

    Update Base.java

--- calculator/Base.java
+++ calculator/Base.java
@@ -1,65 +1,81 @@
 public class Base {
     double Addition(String equation){
         int point = equation.indexOf("+");
+        String eq1;
+        String eq2;
+        double st1;
+        double st2;
         if (!equation.substring(0, point).contains(".")) {
-            String eq1 = equation.substring(0, point) + ".0";
-            double st1 = Double.parseDouble(eq2);
+            eq1 = equation.substring(0, point) + ".0";
+            st1 = Double.parseDouble(eq1);
         } else {
-            double st1 = Double.parseDouble(equation.substring(point + 1));
+            st1 = Double.parseDouble(equation.substring(point + 1));
         }
         if (!equation.substring(point + 1).contains(".")) {
-            String eq2 = equation.substring(point + 1) + ".0";
-            double st2 = Double.parseDouble(eq2);
+            eq2 = equation.substring(point + 1) + ".0";
+            st2 = Double.parseDouble(eq2);
         } else {
-            double st2 = Double.parseDouble(equation.substring(point + 1));
+            st2 = Double.parseDouble(equation.substring(point + 1));
         }
         return((st1 + st2));
     }
     double Subtraction(String equation){
         int point = equation.indexOf("-");
+        String eq1;
+        String eq2;
+        double st1;
+        double st2;
         if (!equation.substring(0, point).contains(".")) {
-            String eq1 = equation.substring(0, point) + ".0";
-            double st1 = Double.parseDouble(eq2);
+            eq1 = equation.substring(0, point) + ".0";
+            st1 = Double.parseDouble(eq1);
         } else {
-            double st1 = Double.parseDouble(equation.substring(point + 1));
+            st1 = Double.parseDouble(equation.substring(point + 1));
         }
         if (!equation.substring(point + 1).contains(".")) {
-            String eq2 = equation.substring(point + 1) + ".0";
-            double st2 = Double.parseDouble(eq2);
+            eq2 = equation.substring(point + 1) + ".0";
+            st2 = Double.parseDouble(eq2);
         } else {
-            double st2 = Double.parseDouble(equation.substring(point + 1));
+            st2 = Double.parseDouble(equation.substring(point + 1));
         }
         return((st1 - st2));
     }
     double Multiplication(String equation){
         int point = equation.indexOf("*");
+        String eq1;
+        String eq2;
+        double st1;
+        double st2;
         if (!equation.substring(0, point).contains(".")) {
-            String eq1 = equation.substring(0, point) + ".0";
-            double st1 = Double.parseDouble(eq2);
+            eq1 = equation.substring(0, point) + ".0";
+            st1 = Double.parseDouble(eq1);
         } else {
-            double st1 = Double.parseDouble(equation.substring(point + 1));
+            st1 = Double.parseDouble(equation.substring(point + 1));
         }
         if (!equation.substring(point + 1).contains(".")) {
-            String eq2 = equation.substring(point + 1) + ".0";
-            double st2 = Double.parseDouble(eq2);
+            eq2 = equation.substring(point + 1) + ".0";
+            st2 = Double.parseDouble(eq2);
         } else {
-            double st2 = Double.parseDouble(equation.substring(point + 1));
+            st2 = Double.parseDouble(equation.substring(point + 1));
         }
         return((st1 * st2));
     }
     double Division(String equation){
         int point = equation.indexOf("/");
+        String eq1;
+        String eq2;
+        double st1;
+        double st2;
         if (!equation.substring(0, point).contains(".")) {
-            String eq1 = equation.substring(0, point) + ".0";
-            double st1 = Double.parseDouble(eq2);
+            eq1 = equation.substring(0, point) + ".0";
+            st1 = Double.parseDouble(eq1);
         } else {
-            double st1 = Double.parseDouble(equation.substring(point + 1));
+            st1 = Double.parseDouble(equation.substring(point + 1));
         }
         if (!equation.substring(point + 1).contains(".")) {
-            String eq2 = equation.substring(point + 1) + ".0";
-            double st2 = Double.parseDouble(eq2);
+            eq2 = equation.substring(point + 1) + ".0";
+            st2 = Double.parseDouble(eq2);
         } else {
-            double st2 = Double.parseDouble(equation.substring(point + 1));
+            st2 = Double.parseDouble(equation.substring(point + 1));
         }
         return((st1/st2));
     }
