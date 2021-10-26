7cf6febbb4b49f2fe77d9a50894ad1ab800f8f9f
ChrisFerrer23145 <70284666+ChrisFerrer23145@users.noreply.github.com>
2021-10-26 11:13:41 -0400

    Update Extra.java

--- calculator/Extra.java
+++ calculator/Extra.java
@@ -1,13 +1,25 @@
 public class Extra {
     Components c = new Components();
     double SendOff(String equation, String order) {
-            equation = c.Handle(eq, order);
-            int eqs = equation.indexOf("=");
-            equation = c.MDo(equation.substring(0, eqs), equation.substring(eqs + 1));
-            eqs = equation.indexOf("=");
-            equation = c.ASo(equation.substring(0, eqs), equation.substring(eqs + 1));
-            double equation2 = Double.parseDouble(equation);
-            return equation2;
+        int i = 0;
+        for(i=0;i<order.length();i++) {
+            if (order.contains("(")){
+                int x = equation.indexOf("(");
+                int y = equation.indexOf(")");
+                String eq = equation.substring(x + 1, y);
+                for(inc=0;inc < eq.length();inc++) {
+				    String ordx = c.Determine(Character.toString(eq.charAt(inc)), "");
+			    }
+                equation = equation.substring(0, x) + SendOff(eq, ordx) + equation.substring(y + 1);
+            }
+        }
+        equation = c.Handle(eq, order);
+        int eqs = equation.indexOf("=");
+        equation = c.MDo(equation.substring(0, eqs), equation.substring(eqs + 1));
+        eqs = equation.indexOf("=");
+        equation = c.ASo(equation.substring(0, eqs), equation.substring(eqs + 1));
+        double equation2 = Double.parseDouble(equation);
+        return equation2;
     }
     double Sqrt(String equation){
         String eq2;
