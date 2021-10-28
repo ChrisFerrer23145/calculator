0b2dfc4f0e85b82de046a1df325a7f44be3d3cb7
ChrisFerrer23145 <70284666+ChrisFerrer23145@users.noreply.github.com>
2021-10-28 12:50:06 -0400

    Update Main.java

--- calculator/Main.java
+++ calculator/Main.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
+	    Extra e = new Extra();
+	    Base b = new Base();
+	    Components c = new Components();
+	   // System.out.println(e.SendOff("(1+(20)-16/4^2)/((5-3)^2+12/2)", ""));
 		int i = 0;
 		for (i=0;i<i+1;i++) {
         	Scanner myobj = new Scanner(System.in);
 			if (i==0) {
-        			System.out.println("Enter equation. For addition, use a '+' between each number. For subtraction, use a '-'. For multiplication use '*', and for division use '/'. Square roots use percents, so '%25' prints '5.0'. Exponents use '^', so '5^3' prints '125.0'. For example, to do addition, you would use '691+234', which would print '925.0'. You can even combine equations, and the program will sort it out in order of operations! For example, '25*5/5+10*2' will print '45.0'! Decimals can be taken in as any number, and every response is printed as a decimal!");
+        	    System.out.println("Enter equation. For addition, use a '+' between each number. For subtraction, use a '-'. For multiplication use '*', and for division use '/'. Square roots use percents, so '%25' prints '5.0'. Exponents use '^', so '5^3' prints '125.0'. For example, to do addition, you would use '691+234', which would print '925.0'. You can even combine equations, and the program will sort it out in order of operations! For example, '25*5/5+10*2' will print '45.0'! Decimals can be taken in as any number, and every response is printed as a decimal!");
 			} else {
 				System.out.println("Enter next equation");
 			}
 			String eq = myobj.nextLine();
-        	Base b = new Base();
-			Extra e = new Extra();
-			Components c = new Components();
 			int inc = 0;
 			// String[] flags = {"%", "^", "*", "/", "+", "-", "(", ")"};
 			String order = "";
@@ -23,7 +24,7 @@ public class Main {
 			counter = order.length();
 			if (counter > 1) {
 				double equation = e.SendOff(eq, order);
-                		System.out.println(equation);
+                System.out.println(equation);
 			}
 			else if (eq.contains("%")) {
 				System.out.println(e.Sqrt(eq));
